6a4c8af1c40bc60bed9722b58b57999d21a11a85
Manash <manashlamichhan5@gmail.com>
2026-06-07 14:26:48 +0545

    feat(backend): mongodb connection

--- backend/index.ts
+++ backend/index.ts
@@ -1,7 +1,15 @@
 import app from "./src/app";
 import {PORT } from './src/config/constant'
+import { connectToMongoDB } from "./src/database/mongo-db";
 
-
+connectToMongoDB()
+    .then(() => {
+        console.log("MongoDB connection established, starting server...");
+    })
+    .catch((error) => {
+        console.error("Failed to connect to MongoDB, server not started.", error);
+        process.exit(1); // Exit the process with an error code
+});
 
 app.listen(
     PORT,
