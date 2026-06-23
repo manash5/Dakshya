9e7404ea32c75749e55275c388b0b280251113c3
Manash <manashlamichhane5@gmail.com>
2026-06-23 11:11:56 +0545

    feat: add whoami, profile update, and password change methods

--- frontend/lib/api/auth.ts
+++ frontend/lib/api/auth.ts
@@ -26,3 +26,60 @@ export const login = async (data: any) => {
         )
     }
 }
+
+export const whoami = async ()=>{
+    try{
+        const response = await axiosInstance.get(API.AUTH.WHOAMI);
+        return response.data;
+        // response.data -> response ko body
+    }catch (error: any) {
+        throw new Error(
+            error?.response?.data?.message || 'Fetch user info failed'
+        );
+    }
+}
+
+export const profileUpdate = async ( data: any) => {
+    try{
+        const response = await axiosInstance.put(
+            API.AUTH.UPDATE, 
+            data,
+            {
+                headers: {
+                    "Content-Type": "multipart/form-data", // for multer
+                }
+            }
+        );
+        return response.data;
+        // response.data -> response ko body
+    }catch (error: any) {
+        throw new Error(
+            error?.response?.data?.message || 'Profile update failed'
+        );
+    }
+}
+
+export const getProfile = async () => {
+    try {
+        const response = await axiosInstance.get(API.AUTH.GET_PROFILE);
+        return response.data; // Returns the user profile object containing name, role, avatarUrl, etc.
+    } catch (error: any) {
+        console.error("AXIOS ERROR DETAILS:", error.response?.status, error.response?.data);
+        throw new Error(
+            error?.response?.data?.message || 'Failed to fetch profile'
+        );
+    }
+}
+
+
+// checks password for update
+export const changePassword = async (data: { currentPassword: string; newPassword: string }) => {
+    try {
+        const response = await axiosInstance.put(API.AUTH.CHANGE_PASSWORD, data);
+        return response.data;
+    } catch (error: any) {
+        throw new Error(
+            error?.response?.data?.message || 'Password change failed'
+        );
+    }
+};
