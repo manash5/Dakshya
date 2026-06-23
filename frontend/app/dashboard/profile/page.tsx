19f6040bc8b6c53cb669175130d17391d94cd84a
Manash <manashlamichhane5@gmail.com>
2026-06-23 15:00:12 +0545

    feat: implement profile page

---
+++ frontend/app/dashboard/profile/page.tsx
@@ -0,0 +1,19 @@
+import { getUserData } from "@/lib/actions/auth-action";
+import { notFound } from "next/navigation";
+import UpdateForm from "./_components/UpdateUser";
+
+export default async function Page() {
+    const userData = await getUserData();
+    if (!userData.success) {
+        throw new Error(userData.message 
+            || "Failed to fetch user data");
+    }
+    if (!userData.data) {
+        notFound();
+    }
+    return (
+        <div className="bg-[#F7F8F5]">
+            <UpdateForm user={userData.data} />
+        </div>
+    );
+}
