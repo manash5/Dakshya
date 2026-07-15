173d13685415f6cbb031dccad89e64d278229980
manash <manashlamichhane5@gmail.com>
2026-07-15 13:04:34 +0545

    fix: mongo db config name

--- backend/src/config/constant.ts
+++ backend/src/config/constant.ts
@@ -4,6 +4,6 @@ dotenv.config();
 export const PORT=process.env.PORT||8088;
 export const baseUrl = process.env.BASE_URL || 'http://localhost:8088';
 export const MOCK_DB=process.env.MOCK_DB||"mock";
-export const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost:27017/dakshya";
+export const MONGODB_URI = process.env.MONGO_URL || "mongodb://localhost:27017/dakshya";
 export const JWT_KEY = process.env.SECRET_KEY || "merosecretjwtkey"; 
 
